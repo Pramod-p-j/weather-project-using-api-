7ed9d1389309626f893c7e26229a291fc072e97b
Pramod-p-j <pramodpushpa12@gmail.com>
2022-08-25 20:26:23 +0530

    modified html and script and style

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ function displayWeather(){
     function fetching(){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=3f57d6d7e2864e67f4e1acd781da6291')
             .then(res => res.json())
+            // .then(data => console.log(data))
             .then(data => {
                 nameValue.textContent = data['name'] ;
                    
-                maxValue.textContent = data['main']['temp_max'];
-                minValue.textContent = data['main']['temp_min'];
-                humidity.textContent = data['main']['humidity'];
-                windSpeed.textContent = data['wind']['speed'];
+                maxValue.textContent = `Max Temperature - ${data['main']['temp_max']}`;
+                minValue.textContent = `Min Temperature - ${data['main']['temp_min']}`;
+                humidity.textContent = `Humidity - ${data['main']['humidity']}`;
+                windSpeed.textContent = `Wind Speed - ${data['wind']['speed']}`;
                 // console.log(nameValue , maxValue, minValue, humidity, windSpeed); 
             })
             
